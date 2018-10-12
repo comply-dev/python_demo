7785dbc99489a4025323ac8e12a95a8b92c125df
Mahmoud Abdelkader <mahmoudimus@users.noreply.github.com>
2018-10-12 12:40:05 -0700

    Handle HTTP Based Proxies (#28)

--- demo/processor.py
+++ demo/processor.py
@@ -30,17 +30,16 @@ def charge(payload):
     url = urljoin(root_url, '/charge')
     proxies = {}
     if 'VGS_PROXY_URL' in current_app.config:
-        proxy_url = urlunsplit(
-            ('https',
-             '{PROXY_USERNAME}:{PROXY_PASSWORD}@{PROXY_URL}:{PROXY_PORT}'.format(
-                 PROXY_USERNAME=current_app.config['VGS_PROXY_USERNAME'],
-                 PROXY_PASSWORD=current_app.config['VGS_PROXY_PASSWORD'],
-                 PROXY_URL=current_app.config['VGS_PROXY_URL'],
-                 PROXY_PORT=current_app.config['VGS_PROXY_PORT']
-             ),
-             '', None, None))
-        proxies['https'] = proxy_url
-        proxies['http'] = proxy_url
+        for scheme in ['https', 'http']:
+            proxies[scheme] = urlunsplit(
+                (scheme,
+                 '{PROXY_USERNAME}:{PROXY_PASSWORD}@{PROXY_URL}:{PROXY_PORT}'.format(
+                     PROXY_USERNAME=current_app.config['VGS_PROXY_USERNAME'],
+                     PROXY_PASSWORD=current_app.config['VGS_PROXY_PASSWORD'],
+                     PROXY_URL=current_app.config['VGS_PROXY_URL'],
+                     PROXY_PORT=current_app.config['VGS_PROXY_PORT']
+                 ),
+                 '', None, None))
     r = requests.post(
         url,
         data=h.dumps(payload),
