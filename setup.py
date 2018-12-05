1c5d5b46d14a37009918727f774d56dee198414b
Mahmoud Abdelkader <mabdelkader@gmail.com>
2018-12-04 16:36:48 -0800

    cleanups

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ import sys
 version = '0.0.1'
 
 setup(
-    name='homepage',
+    name='demo',
     author='Joe Viveiros',
     author_email='joe.viveiros@verygood.systems',
     version=version,
@@ -14,9 +14,12 @@ setup(
     packages=['.'],
     install_requires=[
         "gunicorn==19.7.1",
-        "requests==2.18.4",
+        "requests>=2.20.0,<3.0",
         "Flask==0.12.4",
         "Flask-Admin==1.5.1",
         "Flask-SQLAlchemy==2.3.2",
+        "Flask-Login==0.4.1",
+        "Flask-WTF>=0.14,<0.15",
+        "flask-mail>=0.9.1,<0.10.0",
    ],
 )
